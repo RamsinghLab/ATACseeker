085998738cad8127d1d6a94a21a3e45863edc760
ttriche <tim.triche@gmail.com>
2016-04-07 14:46:04 -0700

    fixing up getEsts/getComplexity and plotComplexity

---
+++ R/getEsts.R
@@ -0,0 +1,23 @@
+#' Estimate the complexity of a library or sample based on unique fragments
+#' using Daley & Smith's implementation of Good & Toulmin's rational function
+#' approximation to solve the missing species problem.
+#'
+#' @param xx      The fragments or sample of fragments
+#' @param withCI  Have preseq compute 95% confidence intervals for plotting?
+#' @param ...     Other arguments to pass on to preseq 
+#' 
+#' @return        A data frame with results
+#' 
+#' @export 
+getEsts <- function(xx, withCI=FALSE, ...) {  
+  if (class(xx) == "GRanges") xx <- getComplexity(xx)
+  message("Estimating complexity (this can take a little while)...")
+  if (!withCI) {
+    res <- as.data.frame(preseqR.rfa.curve(xx, ...)$estimates)
+    names(res) <- c("reads","frags")
+  } else {
+    res <- as.data.frame(preseqR.rfa.species.accum.curve(xx, ..., bootstrap=99))
+    names(res) <- c("reads","frags","frags.lower","frags.upper")
+  }
+  return(res)
+} 
--- R/plotComplexity.R
+++ R/plotComplexity.R
@@ -1,32 +1,29 @@
 #' plot results from preseq (complexity estimation around features)
+#' TODO: use the ... to allow for comparison of multiple libraries
 #'
-#' FIXME: use the ... to allow for comparison of multiple libraries
-#'
-#' @param   x   the thing whose complexity is to be plotted
+#' @param   x       the thing whose complexity is to be plotted
+#' @param   ...     other parameters to pass on to getEsts()
+#' @param   ests    precomputed complexity estimates (speeds up multiple runs) 
+#' @param   withCI  plot confidence intervals for the estimates?  (FALSE) 
+#' @param   add     add to an existing plot? (FALSE) 
 #' 
-#' @return  invisibly, the complexity estimates corresponding to the generated plot
+#' @return  invisibly, the complexity estimates corresponding to the plot
+#' 
+#' @seealso preseqR
 #' 
 #' @export
-plotComplexity <- function(x, ..., withCI=FALSE) {
+plotComplexity <- function(x, ..., ests=NULL, withCI=FALSE, add=FALSE) {
 
-  ## require(preseqR)
-
-  getEsts <- function(xx, withCI=withCI, ...) {  
-    message("Nota bene: this can take a little while.")
-    if (!withCI) {
-      res <- as.data.frame(preseqR.rfa.curve(xx, ...)$estimates)
-      names(res) <- c('reads','frags')
-    } else {
-      res <- as.data.frame(preseqR.rfa.species.accum.curve(xx, ...))
-      names(res) <- c('reads','frags','frags.lower','frags.upper')
+  if (!is.null(ests)) { 
+    if (withCI & !all(c("frags.lower","frags.upper") %in% names(ests))) {
+      stop("CIs requested, but provided estimates do not contain them!")
     }
-    return(res)
-  } 
-
-  if (is(x, 'data.frame')) {
-    res <- getEsts(x, withCI=withCI, ...)
-  } else if (is(x, 'GRanges') | is(x, 'GAlignmentPairs')) {
-    res <- getEsts(getComplexity(x), withCI=withCI, ...)
+    message("Using supplied estimates...")
+    res <- ests
+  } else if (is(x, "data.frame")) {
+    res <- getEsts(x, ..., withCI=withCI)
+  } else if (is(x, "GRanges") | is(x, "GAlignmentPairs")) {
+    res <- getEsts(getComplexity(x), ..., withCI=withCI)
   } else {
     stop("Don't know what to do with a", class(x))
   }
@@ -35,25 +32,48 @@ plotComplexity <- function(x, ..., withCI=FALSE) {
     stop("Complexity estimate failed. You might try estimating from 5' cuts.")
   } else {
     message("Plotting complexity curve", 
-           ifelse(withCI, "with confidence intervals", ""))
+           ifelse(withCI, "with confidence intervals...", "..."))
     if (withCI) { 
+      # {{{
       epsilon <- 100
-      plot(x=res$reads, y=res$frags, 
-           main="Extrapolated library complexity (95% CI)", 
-           pch=NA_integer_, xlab="Unique fragments", ylab="Fragment count")
-      segments(res$reads-epsilon, res$frags.lower, 
-               res$reads+epsilon, res$frags.upper) ## conf. int.
+      if (add == TRUE) { 
+        with(res, 
+             points(x=reads, y=frags, pch=NA_integer_, ...))
+      } else {
+        with(res, 
+             plot(x=reads, y=frags, 
+                  main="Extrapolated library complexity (95% CI)", 
+                  pch=NA_integer_, 
+                  xlab="Read count", 
+                  ylab="Unique fragments"))
+      }
+      with(res, 
+           segments(reads - epsilon, frags.lower, 
+                    reads + epsilon, frags.upper)) ## conf. int.
       points(x=res$reads, y=res$frags, pch=18) ## mean fit 
       lines(x=res$reads, y=res$frags, lwd=3, lty=1, col="red") ## mean fit 
       lines(x=res$reads, y=res$frags.lower, lwd=1, lty=3, col="grey50")
       lines(x=res$reads, y=res$frags.upper, lwd=1, lty=3, col="grey50")
+      # }}}
     } else { 
-      plot(x=res$reads, y=res$frags, col='red', pch=18,
-           main="Extrapolated library complexity",
-           xlab="Unique fragments", ylab="Fragment count")
-      lines(x=res$reads, y=res$frags, lwd=3, lty=3, col="grey50")
+      # {{{
+      if (add == TRUE) { 
+        with(res, 
+             points(x=reads, y=frags, pch=NA_integer_, ...))
+      } else {
+        with(res,
+             plot(x=reads, y=frags, 
+                  col="red", pch=18,
+                  main="Extrapolated library complexity",
+                  xlab="Read count", 
+                  ylab="Unique fragments"))
+      }
+      with(res,
+           lines(x=reads, y=frags, lwd=3, lty=3, col="grey50"))
+      # }}}
     }
   }
   invisible(res)
 
 }
+
