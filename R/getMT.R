00dbddcc56b6bacacb10d2d82068954b5c4df771
ttriche <tim.triche@gmail.com>
2017-07-05 19:23:17 -0700

    mt stuff

--- R/getMT.R
+++ R/getMT.R
@@ -1,14 +1,18 @@
 #' grab the mitochondrial reads from a BAM and estimate their fraction
 #' nb. this could probably be done faster for a list of BAMs but it's not
+#' nb. nb. returns NuMt-depleted mitochondrial GenomicAlignmentPairs 
 #'
 #' @param bam     a BAM filename, which should have an index (else index it)
 #' @param chrM    what the mitochondrial contig is called. Default is "chrM" 
+#' @param flags   (optional) scanBamFlag output for ScanBamParam construction
 #'
 #' @import GenomicAlignments
+#' @import VariantTools
 #' @import Rsamtools
+#' @import gmapR
 #'
 #' @export
-getMT <- function(bam, chrM="chrM") {
+getMT <- function(bam, chrM="chrM", flags=NULL) {
 
   bai <- paste0(bam, ".bai")
   if (!file.exists(bai)) indexBam(bam)
@@ -18,8 +22,20 @@ getMT <- function(bam, chrM="chrM") {
   mtFrac <- idxStats[chrM, "mapped"] / sum(idxStats[, "mapped"])
   mtRange <- GRanges(chrM, IRanges(1, idxStats[chrM, "seqlength"]), "*")
   mtView <- BamViews(bam, bai, bamRanges=mtRange)
-  mtParam <- ScanBamParam(what=c("seq","mate_status"))
-  mtReads <- readGAlignments(mtView, param=mtParam)[[1]]
+
+  # try to exclude NuMt
+  if (is.null(flags)) {
+    flags <- scanBamFlag(isPaired=TRUE, 
+                         isProperPair=TRUE, 
+                         isUnmappedQuery=FALSE, 
+                         hasUnmappedMate=FALSE, 
+                         isSecondaryAlignment=FALSE, 
+                         isNotPassingQualityControls=FALSE, 
+                         isDuplicate=FALSE)
+  }
+
+  mtParam <- ScanBamParam(flag=flags, what=c("seq")) 
+  mtReads <- suppressWarnings(readGAlignments(mtView, param=mtParam)[[1]])
   attr(mtReads, "mtFrac") <- mtFrac
   return(mtReads)
 
